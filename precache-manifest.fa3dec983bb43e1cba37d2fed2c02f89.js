45d5c19576bf595bb5dd391ea7b20d8f308bc282
Piotr Witkos <piotrek@Piotrs-MacBook-Pro.local>
2020-10-11 22:55:39 +0200

    Updates

---
+++ precache-manifest.fa3dec983bb43e1cba37d2fed2c02f89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c33b362cce562632fb31",
+    "url": "/react-router-firebase-project1/static/css/main.263aa060.chunk.css"
+  },
+  {
+    "revision": "c33b362cce562632fb31",
+    "url": "/react-router-firebase-project1/static/js/main.d3f414c1.chunk.js"
+  },
+  {
+    "revision": "c19165403f712aaac3dc",
+    "url": "/react-router-firebase-project1/static/js/runtime~main.990afd4f.js"
+  },
+  {
+    "revision": "9a88b7e942a8c3552674",
+    "url": "/react-router-firebase-project1/static/js/2.c9a77617.chunk.js"
+  },
+  {
+    "revision": "6fd608dff3966f2ec22351ec5e27c8e9",
+    "url": "/react-router-firebase-project1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb02c3ade41cf40725b01768eed9164e.js"
+  "/react-router-firebase-project1/precache-manifest.fa3dec983bb43e1cba37d2fed2c02f89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-router-firebase-project1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
