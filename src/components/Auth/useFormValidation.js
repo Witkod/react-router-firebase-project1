c719a4979f8baa17049aafdd318c810c811276ad
Piotr Witkos <piotrek@Piotrs-MacBook-Pro.local>
2020-10-11 18:13:54 +0200

    initial commit

--- src/components/Auth/useFormValidation.js
+++ src/components/Auth/useFormValidation.js
@@ -1,5 +1,52 @@
 import React from "react";
 
-function useFormValidation() {}
+function useFormValidation(initialState, validate, authenticate) {
+  const [values, setValues] = React.useState(initialState);
+  const [errors, setErrors] = React.useState({});
+  const [isSubmitting, setSubmitting] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isSubmitting) {
+      const isNoErrors = Object.keys(errors).length === 0;
+      if (isNoErrors) {
+        //console.log("authenticated", values);
+        authenticate();
+        setSubmitting(false);
+      } else {
+        setSubmitting(false);
+      }
+    }
+  }, [errors]);
+
+  function handleChange(event) {
+    event.persist(); //without this we loose info about event too early
+    setValues((previousValues) => ({
+      ...previousValues,
+      [event.target.name]: event.target.value, // aktualizuje wartość pola tylko zgodnego z name
+      //dzieki tego aktualizuje tylko konkretne pole a nie wszystkie
+    }));
+  }
+
+  function handleBlur() {
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const validationErrors = validate(values);
+    setErrors(validationErrors);
+    setSubmitting(true);
+    // console.log(values);
+  }
+  return {
+    handleSubmit,
+    handleBlur,
+    handleChange,
+    values,
+    errors,
+    isSubmitting,
+  };
+}
 
 export default useFormValidation;
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,7 +1,91 @@
 import React from "react";
+import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
+import { LINKS_PER_PAGE } from "../../utils";
 
 function LinkList(props) {
-  return <div>LinkList</div>;
+  const { firebase } = React.useContext(FirebaseContext);
+  const [links, setLinks] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [cursor, setCursor] = React.useState(null);
+  const isNewPage = props.location.pathname.includes("new");
+  const isTopPage = props.location.pathname.includes("top");
+  const page = Number(props.match.params.page);
+
+  React.useEffect(() => {
+    const unsubscribe = getLinks();
+    return () => unsubscribe();
+  }, [isTopPage, page]);
+
+  function getLinks() {
+    setLoading(true);
+    const hasCursor = Boolean(cursor);
+    if (isTopPage) {
+      return firebase.db
+        .collection("links")
+        .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (page === 1) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    } else if (hasCursor) {
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    }
+  }
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    });
+    // console.log(links);
+    const lastLink = links[links.length - 1];
+    setLinks(links);
+    setCursor(lastLink);
+    setLoading(false);
+  }
+
+  function visitPreviousPage() {
+    if (page > 1) {
+      props.history.push(`/new/${page - 1}`);
+    }
+  }
+
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE) {
+      props.history.push(`/new/${page + 1}`);
+    }
+  }
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
+  return (
+    <div style={{ opacity: loading ? 0.25 : 1 }}>
+      {links.map((link, index) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + pageIndex}
+        />
+      ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default LinkList;
